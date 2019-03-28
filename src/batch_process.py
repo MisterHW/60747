1b412c04d88c4fe23a036f2650d0e9198d901abe
MisterHW <helgewurst@web.de>
2019-03-28 17:59:38 +0100

    begin of refactoring for multiple devices and experiments

--- src/batch_process.py
+++ src/batch_process.py
@@ -6,12 +6,14 @@ import evaluate_waveform
 
 args = None
 
+
 def extant_file(x):
 	# https://stackoverflow.com/questions/11540854/file-as-command-line-argument-for-argparse-error-message-if-argument-is-not-va
     if not os.path.exists(x):
         # Argparse uses the ArgumentTypeError to give a rejection message
         raise argparse.ArgumentTypeError("{0} does not exist".format(x))
     return x
+	
 	
 def extant_dir(x):
 	x = os.path.abspath(x)
@@ -24,7 +26,7 @@ def init_argparse():
 	global args	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-d", "--directory", type=extant_dir, help="starting path for processing.", required=True)
-	parser.add_argument("-m", "--method", type=int, help="Analysis method (2: diode reverse recovery, 9: IGBT switching characteristics.", required=True)
+	parser.add_argument("-m", "--method", type=string, help="Analysis method (folder name of a template in methods/).", required=True)
 	parser.add_argument("-r", "--recursive", action='store_true', default=False, help='recursively process subdirectories.')
 	parser.add_argument("-o", "--output", default='analysis_result.csv', help='output filename for a .csv with results. Absolute path or relative to -d.')
 	parser.add_argument("-l", "--headerlines", type=int, help='number of header lines to be processed separately (and skipped to jump to the data).', required=True)
@@ -39,9 +41,10 @@ def process_files(start_dir, recursive, method):
 	if os.path.exists(outp):
 		os.remove(outp) 
 		print('deleted %s' % outp)
+		
 	evaluate_waveform.store_header(outp, method)
 	
-	# iterate over all suitable files in directory. 
+	# iterate over all suitable files in start_dir. 
 	# Recursively process subdirectories if required.
 	for f in sorted(os.listdir(start_dir)):
 		f = os.path.join(start_dir, f)
--- src/test9.py
+++
@@ -1,7 +0,0 @@
-#!python3
-
-import evaluate_waveform
-
-filen = '../HP2-02-SW6-2R/18-12-17 15-22-51  100 130 .txt'
-
-evaluate_waveform.process_file(filen, headerlines=22, method=9)
