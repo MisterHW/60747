d5d31867e294ad1fb4d30b5c810bd85d408cda17
MisterHW <helgewurst@web.de>
2019-04-01 18:59:24 +0200

    refactors evaluate_waveform.py for -2 method (executes with test data). Implementation incomplete. Suggests creating a waveform analyzer base class.

--- src/methods/2/evaluate_waveform.py
+++ src/methods/2/evaluate_waveform.py
@@ -3,148 +3,220 @@
 import os
 import sys 
 import numpy as np
-import wfa
 import re
 
-# output table format
-output_table_line_template = \
-	'{success};{Modul};{Schalter};{R_shunt};"{file_base}";' + \
-	'{V_CE_turnoff};{V_DC};{Ipk_turnoff};{Temp.};{RG};{V_GE_high};{V_GE_low};' + \
-	'{turn_off_t1};{turn_off_t2};{turn_off_t3};{turn_off_t4};{turn_on_t1};{turn_on_t2};{turn_on_t3};{turn_on_t4};' + \
-	'{E_turnoff_J};{E_turnon_J};{turn_off_t3};{turn_off_t4};' + \
-	'{I_droop_during_pause};'	
-	
-	
 same_path_as_script = lambda filename: os.path.join(os.path.dirname(__file__), filename)
-plotfile_template = same_path_as_script('gnuplot_template.plt')
+sys.path.append(same_path_as_script('../../'))
+import wfa
 
-CH = []
+import importlib
+# import later: waveform_import
+# import later: preprocess_data
+	
 
-# higher-level analysis description paramters and results in dictionary form
-hdr = {} # header data (dict)
-par = {} # analysis parameters (dict)
-res = {} # results (dict)
-err = {} # errors / quantities that failed to evaluate (contains defaults or NaN)
-	
-	
-def extract_voltage_and_current_values_m2():
-	global CH, par, res
-	
-	print("\tvoltage and current levels")
-	# extract basic waveform voltages and currents
+class analysisData:
+	def __init__(self):
+		self.args = None
+		self.CH = []
+		# higher-level analysis description paramters and results in dictionary form
+		self.hdr = {} # header data (dict)
+		self.par = {} # analysis parameters (dict)
+		self.res = {} # results (dict)
+		self.err = {} # errors / quantities that failed to evaluate (contains defaults or NaN)
 
-	res['I_1st_fr_peak'] = CH[par['CH_ID']].percentile_value(par['tAOI_1st_fr_event'], 0.99)	
-	res['V_D_1st_fr_peak'] = CH[par['CH_VD']].percentile_value(par['tAOI_1st_fr_event'], 0.99)	
-	res['V_D_1st_on_av'] = CH[par['CH_VD']].average(par['tAOI_D_FWD'])[0]
-	res['V_DC_1st_on_av'] = CH[par['CH_VDC']].average(par['tAOI_D_FWD'])[0]
-	res['I_rr_fwd'] = CH[par['CH_ID']].percentile_value(par['tAOI_rr_event'], 0.995)
-	res['I_rr_rev_max'] = CH[par['CH_ID']].percentile_value(par['tAOI_rr_event'], 0.001)		
-	
-	res['I_1st_on_fit_a_bx']  = CH[par['CH_ID']].lin_fit(par['tAOI_D_FWD'])
-	I1 = lambda t, a=res['I_1st_on_fit_a_bx'][0], b=res['I_1st_on_fit_a_bx'][1] : [t, a + b*t]
-	res['I_1st_fr_peak_lin_estimate'] = I1(par['t_1st_fall_nom'])
-	res['I_rr_fwd_lin_estimate'] = I1(par['t_2nd_rise_nom'])
- 	# TODO	
-	
+	def clean_up(self):
+		for c in self.CH:
+			del c
+		self.CH  = []
+		self.hdr = {} 
+		self.par = {} 
+		self.res = {}
+		self.err = {}	
 		
-def store_header(fn, method):
-	line = output_table_line_templates[method].replace('"{','"').replace('}"','"').replace('{','"').replace('}','"') + '\n'
-	if fn == None:
-		print(line)
-	else:
-		f = open(fn, 'w')
-		f.write(line)
-		f.close()
-	return
 
-
-def store_results(fn, method):
-	line = resolve_placeholders(output_table_line_templates[method]) + '\n'
-	if fn == None:
-		print(line)
-	else:
-		f = open(fn, 'a')
-		f.write(line)
-		f.close()
-	return
+class analysisProcessor:
 	
-	
-def __visualize_output(plotfile_template, purge_unresolved_placeholders = False):
-	### generate output and gnuplot file for documentation
-
-	print_params_and_results()
+	def __init__(self, args):
+		self.data = analysisData()
+		self.data.args = args
+		self.output_table_line_template = \
+			'{success};{Modul};{Schalter};{R_shunt};"{file_base}";' + \
+			'{V_CE_turnoff};{V_DC};{Ipk_turnoff};{Temp.};{RG};{V_GE_high};{V_GE_low};' + \
+			'{turn_off_t1};{turn_off_t2};{turn_off_t3};{turn_off_t4};{turn_on_t1};{turn_on_t2};{turn_on_t3};{turn_on_t4};' + \
+			'{E_turnoff_J};{E_turnon_J};{turn_off_t3};{turn_off_t4};' + \
+			'{I_droop_during_pause};'	
+		self.plotfile_template = same_path_as_script('../../setups/%s/gnuplot_template.plt' % args.setup)
 		
-	par['insertion_before_plot'] = ''
-	par['insertion_after_plot']  = ''
 	
-	if len(err) > 0:
-		par['insertion_before_plot'] += 'set label 10000 "{/:Bold FAILED: %s}" font "Verdana,16" tc rgb "red" at graph 0.5, graph 0.5 center front\n' % (", ".join(err.keys()).replace('_', '\\\_'))
-	
-	# generate gnuplot script for visualization and validation
-	f = open(plotfile_template, 'r', encoding='cp1252')
-	plt = f.readlines()
-	f.close()
-	plt_output_filename = par['file_root'] + '.plt'
-	f = open(plt_output_filename, 'w+')
-	for line in plt: # replace all placeholders in the template file (same names as the dictionary keys) 
-		f.write(resolve_placeholders(line, purge_unresolved_placeholders))
-	f.close()	
-	
-def visualize_output( purge_unresolved_placeholders = False):
-	__visualize_output(plotfile_template, purge_unresolved_placeholders)
+	def extract_voltage_and_current_values(self):
+		d = self.data
 		
-		
-def print_assertion_error(error, full_info = False):
-	print('\n\nAssertionError: ', error)
-	if full_info:
-		print('=============================================')
-		visualize_output(purge_unresolved_placeholders = True)
-		print('=============================================\n\n')			
+		print("\tvoltage and current levels")
+		# extract basic waveform voltages and currents
 
-def process_file(filename, headerlines):	
-	global par, res, CH 
-	result = False
-	
-	if read_file_header_and_data(filename):
-		try: 
-			assign_advanced_analysis_parameters_m2()
-			CH[par['CH_ID']].invert()
-			print("analysis:")
-			extract_voltage_and_current_values_m2()
-		except AssertionError as e:
-			print_assertion_error(e)
-			return result
-
-		result = True
+		d.res['I_1st_fr_peak'] = d.CH[d.par['CH_ID']].percentile_value(d.par['tAOI_1st_fr_event'], 0.99)	
+		d.res['V_D_1st_fr_peak'] = d.CH[d.par['CH_VD']].percentile_value(d.par['tAOI_1st_fr_event'], 0.99)	
+		d.res['V_D_1st_on_av'] = d.CH[d.par['CH_VD']].average(d.par['tAOI_D_FWD'])[0]
+		d.res['V_DC_1st_on_av'] = d.CH[d.par['CH_VDC']].average(d.par['tAOI_D_FWD'])[0]
+		d.res['I_rr_fwd'] = d.CH[d.par['CH_ID']].percentile_value(d.par['tAOI_rr_event'], 0.995)
+		d.res['I_rr_rev_max'] = d.CH[d.par['CH_ID']].percentile_value(d.par['tAOI_rr_event'], 0.001)		
 		
-		try: 
-			extract_rr_timing_markers_m2()
-			calculate_rr_characteristics_m2()
-		except AssertionError as e:
-			result = False
-			print_assertion_error(e)
+		d.res['I_1st_on_fit_a_bx']  = d.CH[d.par['CH_ID']].lin_fit(d.par['tAOI_D_FWD'])
+		I1 = lambda t, a=d.res['I_1st_on_fit_a_bx'][0], b=d.res['I_1st_on_fit_a_bx'][1] : [t, a + b*t]
+		d.res['I_1st_fr_peak_lin_estimate'] = I1(d.par['t_1st_fall_nom'])
+		d.res['I_rr_fwd_lin_estimate'] = I1(d.par['t_2nd_rise_nom'])
+		# TODO	
+				
 			
-		res['success'] = int(len(err) == 0) # 1: success, 0: errors occured.
-		visualize_output_m2(purge_unresolved_placeholders = True)
+	def extract_rr_timing_markers(self):
+		# TODO
+		return
 		
-	print("")
-	return result
+		
+	def calculate_rr_characteristics(self):
+		# TODO
+		return
+		
+			
+	def print_assertion_error(self, error, full_info = False):
+		print('\n\nAssertionError: ', error)
+		if full_info:
+			print('=============================================')
+			visualize_output(purge_unresolved_placeholders = True)
+			print('=============================================\n\n')	
+
+			
+		
+	def store_header(self, fn):
+		line = self.output_table_line_template.replace('"{','"').replace('}"','"').replace('{','"').replace('}','"') + '\n'
+		if fn == None:
+			print(line)
+		else:
+			f = open(fn, 'w')
+			f.write(line)
+			f.close()
+		return
+
+
+	def store_results(self, fn):
+		line = self.resolve_placeholders(self.output_table_line_template) + '\n'
+		if fn == None:
+			print(line)
+		else:
+			f = open(fn, 'a')
+			f.write(line)
+			f.close()
+		return
+		
+		
+	def resolve_placeholders(self, s, purge_unresolved = False):
+		d = self.data 
+		
+		for key in d.hdr.keys():
+			s = s.replace('{%s}'%key, str(d.hdr[key]))
+		for key in d.par.keys():
+			s = s.replace('{%s}'%key, str(d.par[key]))
+		for key in d.res.keys():
+			s = s.replace('{%s}'%key, str(d.res[key]))
+		for key in d.err.keys():
+			s = s.replace('{%s}'%key, str(d.err[key]))	
+		if purge_unresolved:
+			placeholder_pattern = r'\{[^\}\/\\]*\}' # match "{text}" pattern except when text contains \ (gnuplot multiline) or / (gnuplot {/:bold ....} )
+			s = re.sub(placeholder_pattern, '(nan)',s)
+		return s
+			
+		
+	def print_params_and_results(self):
+		d = self.data 
 	
+		print("parameters:")
+		for key in sorted(d.par.keys()):
+			print("\t%s = %s" % (key, repr(d.par[key])))	
+			
+		print("results:")
+		for key in sorted(d.res.keys()):
+			print("\t%s = %s" % (key, repr(d.res[key])))
+			
+		if len(d.err) > 0:
+			print("failed to evaluate:")
+			for key in sorted(d.err.keys()):
+				print("\t%s = %s" % (key, repr(d.err[key])))
+			
+		
+
+	def visualize_output(self, purge_unresolved_placeholders = False):
+		d = self.data 
 	
-def store_header(fn, _args):
-	return
+		### generate output and gnuplot file for documentation
+		
+		self.print_params_and_results()
+			
+		d.par['insertion_before_plot'] = ''
+		d.par['insertion_after_plot']  = ''
+		
+		if len(d.err) > 0:
+			d.par['insertion_before_plot'] += 'set label 10000 "{/:Bold FAILED: %s}" font "Verdana,16" tc rgb "red" at graph 0.5, graph 0.5 center front\n' % (", ".join(d.err.keys()).replace('_', '\\\_'))
+		
+		# generate gnuplot script for visualization and validation
+		f = open(self.plotfile_template, 'r', encoding='cp1252')
+		plt = f.readlines()
+		f.close()
+		plt_output_filename = d.par['file_root'] + '.plt'
+		f = open(plt_output_filename, 'w+')
+		for line in plt: # replace all placeholders in the template file (same names as the dictionary keys) 
+			f.write(self.resolve_placeholders(line, purge_unresolved_placeholders))
+		f.close()
 	
+		
+	def __process_file(self, filename):
 	
-def store_results(fn, _args):
-	return
-	
-	
-def clean_up():
-	global CH, hdr, par, res, err
-	for c in CH:
-		del c
-	CH  = []
-	hdr = {} 
-	par = {} 
-	res = {}
-	err = {}
+		result = False
+		
+		headerlines = 22 # TODO: 
+		self.data.par['header_rows'] = headerlines
+		
+		if waveform_import.read_file_header_and_data(filename, self.data):
+			try: 
+				preprocess_data.assign_advanced_analysis_parameters(self.data)
+				preprocess_data.prepare_data(self.data)
+				print("analysis:")
+				self.extract_voltage_and_current_values()
+			except AssertionError as e:
+				self.print_assertion_error(e)
+				return result
+
+			result = True
+			
+			try: 
+				self.extract_rr_timing_markers()
+				self.calculate_rr_characteristics()
+			except AssertionError as e:
+				result = False
+				self.print_assertion_error(e)
+				
+			self.data.res['success'] = int(len(self.data.err) == 0) # 1: success, 0: errors occured.
+			self.visualize_output(purge_unresolved_placeholders = True)
+			
+		print("")
+		return result
+		
+		
+	def process_file(self, filename):
+		global waveform_import, preprocess_data
+		waveform_import   = importlib.import_module('formats.%s.waveform_import' % self.data.args.inputformat)
+		preprocess_data   = importlib.import_module('setups.%s.preprocess_data'  % self.data.args.setup)
+		
+		print("processing:\n\t'%s'" % filename)
+		
+		preprocess_data.assign_basic_analysis_parameters(self.data)
+
+		result = self.__process_file(filename)
+			
+		print("")
+		return result
+		
+		
+	def clean_up(self):
+		self.data.clean_up()
+
+		
