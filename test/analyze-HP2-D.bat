47e9ae538b8202596c55fa15a69203cee6d864a2
helge <helgewurst@web.de>
2019-04-04 20:10:03 +0200

    update test scripts to selectively delete plots that have the same name as the data files (current convention for automatically generated plots), preserving user defined .plt files in the same directory.

--- test/analyze-HP2-D.bat
+++ test/analyze-HP2-D.bat
@@ -1,5 +1,5 @@
 cls
 @set d="HP2-D"
-@del %d%\*.tmp
-@del %d%\*.plt
+@del "%d%\*.tmp"
+@for %%f in (".\%d%\*.txt") do @del "%d%\%%~nf.plt"
 @..\src\batch_process.py -d %d% -f che2018dyn -s HP2_D -m 2 -o "HP2-D.csv"
--- test/analyze-HP2-IGBT.bat
+++ test/analyze-HP2-IGBT.bat
@@ -1,5 +1,5 @@
 cls
 @set d="HP2-IGBT/"
-@del %d%\*.tmp
-@del %d%\*.plt
+@del "%d%\*.tmp"
+@for %%f in (".\%d%\*.txt") do @del "%d%\%%~nf.plt"
 @..\src\batch_process.py -d %d% -f che2018dyn -s HP2_IGBT -m 9 -o "HP2-IGBT.csv"
